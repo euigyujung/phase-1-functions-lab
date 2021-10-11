f3d50f771da60d866d6505aa48d356d8557ece26
euigyu <euigyu.jung@gmail.com>
2021-10-10 21:15:22 -0700

    commit tree d4c562d802ab3d9ad034e5a32f6ffc069d0a4045

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(bStart){
+    if (bStart > 42)
+       return bStart - 42
+    else  (bStart < 42)
+        return 42 - bStart
+}
+
+function distanceFromHqInFeet(fStart){
+    if (fStart > 42)
+        return (fStart-42)*264;
+    else (fStart < 42)
+        return (42-fStart)*264
+}
+
+function distanceTravelledInFeet(fLocation, dLocation){
+    if (fLocation > dLocation)
+        return (fLocation - dLocation)* 264
+    else if (fLocation < dLocation)
+        return (dLocation - fLocation) * 264
+}
+function calculatesFarePrice(start, desitnation){
+    let blockDistnace = Math.abs(start - desitnation);
+    let distanceTraveled = blockDistnace * 264;
+    if (distanceTraveled < 400){
+        console.log('give customers a free samnple');
+        return 0
+    }
+    else if (distanceTraveled >= 2500)
+        return 'cannot travel that far'
+    else if (2000 < distanceTraveled < 2500)
+        return 25
+    if (400 < distanceTraveled < 2000)
+        return (distanceTraveled * .02)
+}  
+
+
